2b94378a25c20c854dda56f1f4a49a3814eb1cb9
Naceur <NaceurDjebali@gmail.com>
2019-01-09 15:17:02 +0100

    create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,22 @@
+/* add event handler */ 
+
 function submitComment() {
-  console.log('Hello!');
+   const inputField = document.getElementById('name');
+   const name = inputField.value
+   const msg = document.getElementById('msg');
+
+/*creating elements*/
+
+   const comment = document.createElement('section');
+   const h3 = document.createElement('h3');
+   const p = document.createElement('p')
+  console.log();
+
+h3.innerHTML = `${name} said:`
+  p.innerHTML = msg
+  comment.classList.add('comment')
+  comment.appendChild(h3)
+  comment.appendChild(p)
+
+  console.log(comment)
 }
